14e82e911f75ee9b8db3e0f5ab9782583f9c48e6
DESKTOP-CB0EGJG\Nadya <nr@redtower.by>
2021-04-07 22:37:30 +0300

    Add Task3

---
+++ Assets/Scripts/Step.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Step : MonoBehaviour
+{
+   
+   [TextArea] public string contentText;
+   
+   public string titleText;
+
+   public Step[] nextSteps;
+   
+}
---
+++ Assets/Scripts/TextQuest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextQuest : MonoBehaviour
+{
+    public Text contentLablel;
+    public Text titleText;
+    public Step startStep;
+    
+    private Step currentStep;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        currentStep = startStep;
+
+        UpdateContentLable();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            MoveToNextStep(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            MoveToNextStep(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            MoveToNextStep(2);
+        }
+    }
+    private void UpdateContentLable()
+    {
+        titleText.text = currentStep.titleText;
+        contentLablel.text = currentStep.contentText;
+    }
+    private void MoveToNextStep(int stepIndex)
+    {
+        if (currentStep.nextSteps.Length > stepIndex)
+        {
+            currentStep = currentStep.nextSteps[stepIndex];
+            UpdateContentLable();
+        }
+    }
+}
